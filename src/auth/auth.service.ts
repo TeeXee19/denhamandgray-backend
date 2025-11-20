934081ed831ce7249c9e28550190695a8d57fece
Uchechukwu Ottah <usottah@gmail.com>
2025-11-20 17:53:42 +0100

    chore: replace bcrypt with bcryptjs in auth service and prisma admin seed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { LoginDto, LoginResponseData } from './dto/auth.dto';
